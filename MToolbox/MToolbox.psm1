5cbf135f5a78f7f13a9bf24c90fb2824d22cac72
Mike Bennett <fiddymike@gmail.com>
2017-06-14 16:04:25 -0700

    Test remove test line

--- MToolbox/MToolbox.psm1
+++ MToolbox/MToolbox.psm1
@@ -1857,5 +1857,3 @@ Export-ModuleMember -function Remove-MyPSS
 Export-ModuleMember -function New-StoredCredential
 Export-ModuleMember -function Get-StoredCredential
 Export-ModuleMember -alias rcw
-
-#test
